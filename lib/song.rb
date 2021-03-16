f0c30908ec2f66098ab5db3942dbc719302b23d9
rsmith <legendary.belief@gmail.com>
2021-03-15 20:36:45 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+  
+    def self.count
+      @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.each_with_object({}) {|(k, v), hash|  hash[k] = @@genres.count(k) }
+    end
+    
+    def self.artist_count
+        @@artists.each_with_object({}) {|(k, v), hash|  hash[k] = @@artists.count(k) }
+    end
+
+    def initialize(name, artist, genre)
+      @@count += 1
+      @@artists << artist
+      @@genres << genre
+      self.name = name
+      self.artist = artist
+      self.genre = genre
+    end
+  
+  end
+  
